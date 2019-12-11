6ed81a04660288c18dff777073ac1044ed42af4d
Jeri <jeri.atkins@student.swic.edu>
2019-12-10 21:59:38 -0600

    fixed Html labels
    fixed labels so I could assign their value to variables

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@
  * add fields from the form
  * create function to add registrants
  */
+
+
 document.querySelector("form").addEventListener("submit", function(event)){
   event.preventDefault();
 };
